a360afb186d4041c2ff56a29fafc29618b24476c
Usuario <saragl728@gmail.com>
2023-11-06 10:25:17 +0100

    Creo el paquete Adapter

---
+++ app/src/main/java/com/example/alquiler_vehiculos/Adapter/VehicleAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.alquiler_vehiculos.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class VehicleAdapter : RecyclerView.Adapter<VehicleViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VehicleViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: VehicleViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/alquiler_vehiculos/Adapter/VehicleViewHolder.kt
@@ -0,0 +1,20 @@
+package com.example.alquiler_vehiculos.Adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.alquiler_vehiculos.Vehiculos
+import com.example.alquiler_vehiculos.databinding.ItemVehiculoBinding
+import
+
+class VehicleViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+val binding = ItemVehiculoBinding.bind(view)
+    fun render(vehicleModel: Vehiculos){
+        //hago que se conecten los datos de la clase con el layout
+        binding.nombreVehiculo.text = vehicleModel.modelo
+        binding.Volumen.text = vehicleModel.capacidad
+        binding.numeroPlazas.text = vehicleModel.numeroPlazas
+        Glide.with(binding.fotoVehiculo.context).load(vehicleModel.urlVehiculo).into(binding.fotoVehiculo)
+    }
+
+}
